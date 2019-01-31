343d5071ba82ee3405019f29fa3600e66f357075
yang <364334437@qq.com>
2019-01-31 19:17:30 +0800

    start

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Author: yang
+ * @Date: 2019/1/31 19:13
+ * @Version 1.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+        System.out.println("start");
+    }
+}
