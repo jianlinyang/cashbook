062e3b434a800ca8e23079e6172997aa4d5a97a6
yang <364334437@qq.com>
2019-01-31 22:06:44 +0800

    start

--- account-core/src/main/java/com/shu/Application.java
+++ account-core/src/main/java/com/shu/Application.java
@@ -1,13 +1,17 @@
+package com.shu;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 /**
  * @Author: yang
- * @Date: 2019/1/31 19:13
+ * @Date: 2019/1/31 21:41
  * @Version 1.0
  */
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
-        System.out.println("=================start=================");
     }
 }
