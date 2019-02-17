bd9c64fbd8a9093fb2c5f89ab244d11329a7b6b4
yang <364334437@qq.com>
2019-02-17 20:28:28 +0800

    更新用户注册controller

--- src/main/java/com/shu/cashbook/controller/IndexController.java
+++ src/main/java/com/shu/cashbook/controller/IndexController.java
@@ -1,43 +1,41 @@
 package com.shu.cashbook.controller;
 
 import com.shu.cashbook.common.BaseResult;
+import com.shu.cashbook.common.utils.MainUtils;
 import com.shu.cashbook.domain.User;
 import com.shu.cashbook.service.UserService;
-import com.shu.cashbook.common.utils.MainUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Author: yang
  * @Date: 2019/2/4 19:03
  * @Version 1.0
  */
+@Api("用户注册Controller")
 @RestController
 public class IndexController {
-    @Autowired
+    @Resource
     private UserService userService;
 
+    @ApiOperation("用户注册方法")
+    @ApiImplicitParam(name = "user",value = "传入用户对象")
     @PostMapping("regist")
     @Transactional
-    public BaseResult regist(@RequestParam String email,
-                             @RequestParam String name,
-                             @RequestParam(required = false) String level,
-                             @RequestParam(required = false) String icon,
-                             @RequestParam String password) {
-        User byEmail = userService.findByEmail(email);
+    public BaseResult regist(User user) {
+        User byEmail = userService.findByEmail(user.getUserEmail());
         if (null != byEmail) {
             return BaseResult.failed(403,"用户名已存在");
         } else {
-            User user = new User();
             user.setId(MainUtils.getUuid());
-            user.setUserEmail(email);
-            user.setUsername(name);
-            user.setUserLevel(level);
-            user.setUserIcon(icon);
-            user.setPassword(MainUtils.getBCryptStr(password));
+            user.setUserLevel("0");
+            user.setPassword(MainUtils.getBCryptStr(user.getPassword()));
             user.setAuthority("ROLE_USER");
             userService.insert(user);
             return BaseResult.success("注册成功");
